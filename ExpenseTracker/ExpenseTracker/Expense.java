b6180d056b32fec2283b078be9fa17fc8ac8ec1b
NandhiniiSelvam <154488800+NandhiniiSelvam@users.noreply.github.com>
2026-04-16 12:39:10 +0530

    Added Expense.java

---
+++ ExpenseTracker/ExpenseTracker/Expense.java
@@ -0,0 +1,13 @@
+class Expense {
+    String category;
+    double amount;
+
+    Expense(String category, double amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    void display() {
+        System.out.println("Category: " + category + " | Amount: ₹" + amount);
+    }
+}
